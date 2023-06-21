12dcc421ca5b3c6319edabbd697878dc56c73f34
shahriar <shahriarkabir078@gmail.com>
2023-06-21 17:34:46 +0600

    new

---
+++ script.js
@@ -0,0 +1,12 @@
+//for sticky nav bar
+
+// window.onscroll = function() {myFunction()};
+// var navbar = document.querySelector(".nav-wrapper");
+// var sticky = navbar.offsetTop;
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
